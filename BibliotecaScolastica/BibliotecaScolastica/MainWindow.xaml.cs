4f22fd7a2c719bef466d86a07e7e9e4e612b9cf6
Gabriele Ventura <gabriele.ventura.stud@ispascalcomandini.it>
2024-10-21 09:46:49 +0200

    Release 0006: Collegamento xaml con il codice

--- BibliotecaScolastica/BibliotecaScolastica/MainWindow.xaml.cs
+++ BibliotecaScolastica/BibliotecaScolastica/MainWindow.xaml.cs
@@ -16,9 +16,31 @@ namespace BibliotecaScolastica
     /// </summary>
     public partial class MainWindow : Window
     {
+        Biblioteca biblioteca = new Biblioteca();
+
         public MainWindow()
         {
             InitializeComponent();
+            lbl_NomeBiblioteca.Text = biblioteca.NomeBiblioteca;
+        }
+
+        private void btn_Aggiungi_Click(object sender, RoutedEventArgs e)
+        {
+            libro = new Libro(txt_Autore.Text, txt_Titolo.Text, Convert.ToInt32(txt_Anno.Text), txt_Editore.Text, Convert.ToInt32(txt_Pagine.Text));
+            biblioteca.AggiungiLibro(libro);
+        }
+
+        private void btn_Cerca_Click(object sender, RoutedEventArgs e)
+        {
+            string risultato = biblioteca.CercaTitolo(txt_CercaTitolo.Text);
+            if (risultato != null)
+            {
+                txt_Risultato.Text = risultato;
+            }
+            else
+            {
+                txt_Risultato.Text = "Libro non trovato";
+            }
         }
     }
 }
